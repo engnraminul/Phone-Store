315ae523baaac5af3e1090dffd2f2756d0575e88
engnraminul <engnraminul@gmail.com>
2022-08-05 21:00:28 +0600

    Create Phone Specifications Model

--- Phone/admin.py
+++ Phone/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ProcessorBrand, Processor, PhoneBrand
+from .models import ProcessorBrand, Processor, PhoneBrand, Phone
 
 admin.site.register(ProcessorBrand)
 admin.site.register(Processor)
 admin.site.register(PhoneBrand)
+admin.site.register(Phone)
--- Phone/models.py
+++ Phone/models.py
@@ -1,6 +1,9 @@
+from codecs import charmap_build
 from distutils.command.upload import upload
 from enum import unique
+from platform import platform
 from pyexpat import model
+from turtle import color
 from unicodedata import category
 from django.db import models
 from django.forms import CharField, SlugField
@@ -86,5 +89,94 @@ class Phone(models.Model):
     annoucement = models.DateField()
     release = models.DateField()
 
-    processor = models.ForeignKey(Processor, on_delete=models.PROTECT, null=True, blank=True)
+    #Network
+    network_type = models.CharField(max_length=100)
+    network_speed = models.CharField(max_length=100)
+    sim = models.CharField(max_length=50)
+    two_g= models.BooleanField(default=True)
+    two_g_band = models.CharField(max_length=100)
+    three_g =models.BooleanField(default=True)
+    three_g_band = models.CharField(max_length=100)
+    four_g = models.BooleanField(default=False)
+    four_g_band = models.CharField(max_length=100)
+    five_g = models.BooleanField(default=False)
+    five_g_band = models.CharField(max_length=100)
+    six_g = models.BooleanField(default=False)
+    six_g_band = models.CharField(max_length=100)
+    seven_g = models.BooleanField(default=True)
+    seven_g_band = models.CharField(max_length=100)
 
+    #Body
+    dimension = models.CharField(max_length=100)
+    weight = models.CharField(max_length=20)
+    front = models.CharField(max_length=50)
+    back = models.CharField(max_length=50)
+    frame = models.CharField(max_length=50)
+    color = models.CharField(max_length=100)
+    body_feature = models.TextField()
+
+    #Display
+    display_type = models.CharField(max_length=100)
+    size = models.CharField(max_length=20)
+    resulation = models.CharField(max_length=50)
+    ratio = models.CharField(max_length=20)
+    pixel_density = models.CharField(max_length=20)
+    refresh_rate = models.CharField(max_length=20)
+    protection = models.CharField(max_length=100)
+    display_features = models.CharField(max_length=100)
+
+    #camera
+    back_camera = models.TextField(max_length=None)
+    back_camera_features = models.CharField(max_length=150)
+    back_camera_video = models.CharField(max_length=150)
+
+    front_camera = models.TextField(max_length=None)
+    front_camera_features = models.CharField(max_length=150)
+    front_camera_video = models.CharField(max_length=150)
+
+    #platform
+    os = models.CharField(max_length=50)
+    os_version = models.CharField(max_length=50)
+    ui = models.CharField(max_length=50)
+    processor = models.ForeignKey(Processor, on_delete=models.PROTECT, null=True, blank=True)
+    chipset = models.TextField(max_length=None)
+
+
+    #Memory
+    ram = models.CharField(max_length=100)
+    storage = models.CharField(max_length=100)
+    memory_slot = models.BooleanField(default=False)
+    variant = models.CharField(max_length=50)
+    storage_type = models.CharField(max_length=50)
+
+    #Audio
+    speaker = models.CharField(max_length=100)
+    audio_jack = models.BooleanField(default=False)
+    audio_features = models.CharField(max_length=100)
+
+    #Connectivity
+    wifi = models.CharField(max_length=100)
+    hotspot = models.BooleanField(default=True)
+    nfc = models.BooleanField(default=False)
+    gps = models.BooleanField(default=True)
+    radio = models.BooleanField(default=True)
+    usb = models.CharField(max_length=100)
+    infrared_port = models.BooleanField(default=False)
+
+    #Sensor
+    fingureprint = models.CharField(max_length=100)
+    facelock = models.CharField(max_length=100)
+    sensor = models.CharField(max_length=200)
+
+    #Battery
+    battery_type = models.CharField(max_length=100)
+    battery_capacity = models.CharField(max_length=50)
+    charging = models.CharField(max_length=100)
+    wireless_charging = models.CharField(max_length=100)
+    reverse_charging = models.CharField(max_length=100)
+    charging_features = models.TextField(max_length=None)
+
+    #Price
+    united_states = models.TextField(max_length=None)
+    europe = models.TextField(max_length=None)
+    india = models.TextField(max_length=None)
