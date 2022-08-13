f26613bb3b2a781c188eab058d2742027e149fcb
engnraminul <engnraminul@gmail.com>
2022-08-13 11:35:02 +0600

    Download and setup bootstrap file

--- StoreMain/settings.py
+++ StoreMain/settings.py
@@ -15,6 +15,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
