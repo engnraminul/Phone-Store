8f7972109671249baaa13f5dc682152f32b6b112
engnraminul <engnraminul@gmail.com>
2022-08-24 23:58:23 +0600

    Create blog list

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from .models import *
 
 def blog_list(request):
-    return render(request, 'blog/blog_list.html')
+    blog = Blog.objects.all()
+    print(blog)
+
+    context = {
+        'blog':blog,
+    }
+    return render(request, 'blog/blog_list.html', context)
