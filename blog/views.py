e2f36ab556ce9891d0e2463c07d4f74da8c133f8
engnraminul <engnraminul@gmail.com>
2022-08-26 22:24:10 +0600

    Create pagination in blog list

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
+import imp
 from multiprocessing import context
 from django.shortcuts import render
 from .models import *
+from django.core.paginator import Paginator
 
 def blog_list(request):
     blog = Blog.objects.all()
+    paginator = Paginator(blog, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'blog':blog,
+        'page_obj':page_obj,
     }
     return render(request, 'blog/blog_list.html', context)
 
