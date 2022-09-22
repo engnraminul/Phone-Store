5ec7dc0b4607ab3a66e05560c8647f21f6d7ecfb
engnraminul <engnraminul@gmail.com>
2022-09-22 12:40:37 +0600

    django-redirect-to-non-www

--- StoreMain/settings.py
+++ StoreMain/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'redirect_to_non_www.middleware.RedirectToNonWww',
 ]
 
 ROOT_URLCONF = 'StoreMain.urls'
