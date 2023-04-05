bccf726c6ef76eb934edd12b4c3652726b3c2e0b
engnraminul <engnraminul@gmail.com>
2023-04-06 01:50:52 +0600

    create latest processor features

--- Phone/templatetags/popular_phone_tags.py
+++ Phone/templatetags/popular_phone_tags.py
@@ -1,7 +1,7 @@
 from atexit import register
 from django import template
 
-from Phone.models import Phone
+from Phone.models import Phone, Processor
 
 register = template.Library()
 
@@ -11,6 +11,11 @@ def popular_tags(request):
         popular_tags = Phone.objects.filter(status='Released').order_by('-views')[:10]
         return popular_tags
 
+@register.filter
+def processor_tags(request):
+    if request:
+        processor_tags = Processor.objects.order_by('-released')
+        return processor_tags
 
 @register.filter
 def upcoming_popular_tags(request):
